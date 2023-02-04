f436cf29821c909524aec449aaab92480ec81519
Kevin Pedraza <kevinpedraza2003@gmail.com>
2023-02-04 17:47:04 -0500

    agreged change color and ajust size of the timer

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-var Time_Pomodoro = document.getElementById('Time_Pomodoro');
+var colors = document.getElementsByClassName('Color');
+var bodytag = document.getElementById('body');
+
+function ChangeColor(){
+    document.addEventListener('click',function(event){
+        var coloruser = document.getElementById(`${event.target.id}`);
+        var elementstyle = window.getComputedStyle(coloruser);
+        var elementcolor = elementstyle.getPropertyValue('background-color');
+        console.log(elementcolor)
+        bodytag.style.backgroundColor = `${elementcolor}`;
+    })   
+}
+
+var time = document.getElementById('Time_Pomodoro');
