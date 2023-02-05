6dba1a3c691aacf54a5911edecf213d9deb9bd1b
Kevin Pedraza <kevinpedraza2003@gmail.com>
2023-02-04 20:35:56 -0500

    corrected bug in selector of colors

--- js/index.js
+++ js/index.js
@@ -4,10 +4,14 @@ var bodytag = document.getElementById('body');
 function ChangeColor(){
     document.addEventListener('click',function(event){
         var coloruser = document.getElementById(`${event.target.id}`);
-        var elementstyle = window.getComputedStyle(coloruser);
-        var elementcolor = elementstyle.getPropertyValue('background-color');
-        console.log(elementcolor)
-        bodytag.style.backgroundColor = `${elementcolor}`;
+        for(var i of colors){
+            if(i == coloruser){
+                var elementstyle = window.getComputedStyle(coloruser);
+                var elementcolor = elementstyle.getPropertyValue('background-color');
+                console.log(elementcolor)
+                bodytag.style.backgroundColor = `${elementcolor}`;       
+            }
+        }
     })   
 }
 
